e74f587716cd12bd4d1d99a9fc935a823a3e83ce
Kashan Shaikh <shaikhkashan670@gmail.com>
2024-12-01 03:40:15 +0500

    Accout connect

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+// Github Connected
 Route::get('/home', [HomeController::class, 'index'])->middleware('auth')->name('home');
 
 // Route::get('/adminpage', [HomeController::class, 'adminpage'])->middleware(['auth','admin'])->name('adminpage');
